1dcd01eba367daaaabfbf73522a68e1cc019e140
gSayak <gsayak22@gmail.com>
2023-07-28 19:04:57 +0530

    new branch to organise stuff

---
+++ databaseCRUD/src/main/java/com/sayak/databaseCRUD/DatabaseCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sayak.databaseCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseCrudApplication.class, args);
+	}
+
+}
